a3441e2ea5f85528719b6879504734e880cdcd9b
Bruce Herr II <bherr@indiana.edu>
2019-05-16 16:47:49 -0400

    Add seizure as a search term for clinical trials

--- scripts/01-get-ct-data.sql.sh
+++ scripts/01-get-ct-data.sql.sh
@@ -6,17 +6,18 @@ set -ev
 psql << EOF
 
 CREATE TEMPORARY VIEW ctx_locations AS
-  SELECT K.id, official_title as name, acronym, coalesce(start_date_year, 'Unknown') as year, RIGHT(completion_date, '4') AS completion_year, keywords, FF.name as facility, city, state, zip
+  SELECT K.id, official_title as name, acronym, coalesce(start_date_year, 'Unknown') AS year, 
+    RIGHT(completion_date, '4') AS completion_year, keywords, FF.name AS facility, city, state, zip
   FROM (
-    SELECT id, string_agg(keyword, '|') as keywords
+    SELECT id, string_agg(keyword, '|') AS keywords
     FROM ct_keyword 
-    WHERE keyword ilike '%epilepsy%'
+    WHERE keyword ILIKE '%epilepsy%' OR keyword ILIKE '%seizure%'
     GROUP BY id
   ) AS K 
     JOIN ct_fac_address AS F ON (K.id = F.id)
     JOIN ct_facility AS FF ON (F.id = FF.id AND F.loc_ctr = FF.loc_ctr AND F.fid_ctr = FF.fid_ctr)
     JOIN ct_master AS M ON (K.id = M.id)
-  WHERE country = 'United States' AND coalesce(start_date_year, '0')::integer >= 2018;
+  WHERE country = 'United States' AND coalesce(start_date_year, '0')::integer >= ${MINYEAR};
 
 \COPY (SELECT * FROM ctx_locations) TO '${OUT}/ct-locations.raw.csv' Delimiter ',' CSV HEADER Encoding 'SQL-ASCII'
 
