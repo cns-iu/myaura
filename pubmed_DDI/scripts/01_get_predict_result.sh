3ca9ba45ed31fd3d1d06661e5f978f3ce3949ec2
Xuan Wang <code@wangxuan.name>
2020-10-25 16:09:05 -0400

    move data to separate dir

--- pubmed_DDI/scripts/01_get_predict_result.sh
+++ pubmed_DDI/scripts/01_get_predict_result.sh
@@ -1,9 +1,12 @@
-mysql --defaults-file=../../mysql.cnf --defaults-group-suffix=pubmed_ddi <../sql/invivo180717_all.sql >invivo_tmp.txt
-mysql --defaults-file=../../mysql.cnf --defaults-group-suffix=pubmed_ddi <../sql/invitro180612_all.sql >invitro_tmp.txt
-mysql --defaults-file=../../mysql.cnf --defaults-group-suffix=pubmed_ddi <../sql/clinical180717_all.sql >clinical_tmp.txt
+mysql --defaults-file=../mysql.cnf --defaults-group-suffix=pubmed_ddi <../sql/invivo180717_all.sql >invivo_tmp.txt
+mysql --defaults-file=../mysql.cnf --defaults-group-suffix=pubmed_ddi <../sql/invitro180612_all.sql >invitro_tmp.txt
+mysql --defaults-file=../mysql.cnf --defaults-group-suffix=pubmed_ddi <../sql/clinical180717_all.sql >clinical_tmp.txt
 awk '{print $1}' invivo_tmp.txt | sed 1d > invivo_LSVC_180717_allid.txt
 awk '{print $1}' invitro_tmp.txt | sed 1d > invitro_LSVC_180612_allid.txt
 awk '{print $1}' clinical_tmp.txt | sed 1d > clinical_LR_180717_allid.txt
 mv invivo_tmp.txt invivo_all_conf.txt
 mv invitro_tmp.txt invitro_all_conf.txt
 mv clinical_tmp.txt clinical_all_conf.txt
+
+mkdir -p data
+mv *.txt data/
