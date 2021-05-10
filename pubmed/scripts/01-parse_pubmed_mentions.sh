fc10df13047a0b9534f41a8ffa0c54c5749ee984
Xuan Wang <code@wangxuan.name>
2021-05-09 23:23:21 -0400

    Add index

--- pubmed/scripts/01-parse_pubmed_mentions.sh
+++ pubmed/scripts/01-parse_pubmed_mentions.sh
@@ -19,6 +19,13 @@ EOF
 
 python src/parse_pubmed_mentions.py
 
+psql << EOF
+
+CREATE UNIQUE INDEX ON $SCHEMA.$TABLE(pmid);
+CREATE INDEX ON $SCHEMA.$TABLE(year_publication);
+
+EOF
+
 #
 # SQL command to grant permissions to users on the new schema and tables
 #
@@ -29,3 +36,34 @@ do
     psql -c "GRANT ALL ON SCHEMA $SCHEMA TO $USER WITH GRANT OPTION;"
     psql -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA $SCHEMA TO $USER WITH GRANT OPTION;"
 done
+
+
+## set another view adding article title per Larry's request
+## Larry doesn't need this view any more. Leave here in case needed in the future.
+
+#MERGED_TABLE=mention_and_title
+#
+#psql << EOF
+#DROP MATERIALIZED VIEW IF EXISTS $SCHEMA.$MERGED_TABLE;
+#
+#CREATE MATERIALIZED VIEW $SCHEMA.$MERGED_TABLE AS
+#SELECT
+#    m.pmid,
+#    m.pub_year,
+#    m.article_title,
+#    e.match
+#FROM pubmed.view_epilepsy m INNER JOIN $SCHEMA.$TABLE e
+#ON CAST (m.pmid AS INT) = e.pmid;
+#
+#-- Indexes
+#CREATE UNIQUE INDEX ON $SCHEMA.$MERGED_TABLE(pmid);
+#CREATE INDEX ON $SCHEMA.$MERGED_TABLE(pub_year);
+#
+#-- Permissions
+#GRANT ALL ON TABLE $SCHEMA.$MERGED_TABLE TO bherr WITH GRANT OPTION;
+#GRANT ALL ON TABLE $SCHEMA.$MERGED_TABLE TO gallantm WITH GRANT OPTION;
+#GRANT ALL ON TABLE $SCHEMA.$MERGED_TABLE TO rionbr WITH GRANT OPTION;
+#GRANT ALL ON TABLE $SCHEMA.$MERGED_TABLE TO larzhang WITH GRANT OPTION;
+#GRANT ALL ON TABLE $SCHEMA.$MERGED_TABLE TO xw47 WITH GRANT OPTION;
+#
+#EOF
