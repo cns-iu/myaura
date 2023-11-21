2fff4ae9e694e36cbbe626c04173c91893aef2ea
Xuan Wang <code@wangxuan.name>
2023-11-21 00:36:40 -0500

    a hack to add run.sh to old code

---
+++ instagram/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -ev
+
+source ../db-config.sh
+source scripts/var.sh
+
+OLD_DICT_VERSION=20180706
+
+sed -i "s/$OLD_DICT_VERSION/$DICT_VERSION/" src/01-parse_instagram_mentions.py
+sed -i "s/$OLD_DICT_VERSION/$DICT_VERSION/" src/02-count_comentions_samepost.py
+sed -i "s/$OLD_DICT_VERSION/$DICT_VERSION/" src/04-build_network_samepost.py
+sed -i "s/$OLD_DICT_VERSION/$DICT_VERSION/" scripts/04-upload_network.sh
+
+python src/01-parse_instagram_mentions.py
+python src/02-count_comentions_samepost.py
+python src/04-build_network_samepost.py
+
+chmod +x scripts/04-upload_network.sh
+./scripts/04-upload_network.sh
+
+# restore every file back
+sed -i "s/$DICT_VERSION/$OLD_DICT_VERSION/" src/01-parse_instagram_mentions.py
+sed -i "s/$DICT_VERSION/$OLD_DICT_VERSION/" src/02-count_comentions_samepost.py
+sed -i "s/$DICT_VERSION/$OLD_DICT_VERSION/" src/04-build_network_samepost.py
+sed -i "s/$DICT_VERSION/$OLD_DICT_VERSION/" scripts/04-upload_network.sh
