a8684fa57c10ec51949ab253481cc023be89d094
Xuan Wang <code@wangxuan.name>
2026-07-15 01:03:44 -0400

    alt_dict: parametrized rewrite of co-mention counting and network build
    2023 refactor of the co-mention + network-build steps into reusable functions:
    - count_comentions_samepost_alt.py: takes drop_set + save_path, emits per-token
      mention counts; unique-parent-set semantics
    - build_network_samepost_alt.py: reads alt comention + mention-count CSVs,
      computes proximity/distance, both metric AND ultrametric (disjunction=max)
      backbone; normalizes by separate mention counts rather than edge sums.

---
+++ instagram/alt_dict/build_network_samepost_alt.py
@@ -0,0 +1,250 @@
+# coding=utf-8
+# Author: Rion B Correia & Xuan Wang
+# Date: Jan 07, 2021
+#
+# Description: Build the Network from co-mentions
+#
+import os
+import sys
+#
+include_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'include'))
+#include_path = '/nfs/nfs7/home/rionbr/myaura/include'
+sys.path.insert(0, include_path)
+#
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_rows', 40)
+pd.set_option('display.max_columns', 20)
+pd.set_option('display.width', 1000)
+import networkx as nx
+#
+import utils
+from dijkstra import all_pairs_dijkstra_path_length
+#from distanceclosure.utils import _prox2dist as prox2dist
+#from distanceclosure.dijkstra import Dijkstra
+#from distanceclosure.cython._dijkstra import _cy_single_source_shortest_distances
+
+
+def build_instagram_network_samepost(save_path, metric_backbone=True):
+    # Init
+    dicttimestamp = '20180706'
+    # dicttimestamp = '20210321'
+    #window_size = '7'  # in days
+
+    """
+    #
+    # Load DDI
+    #
+    engine = db.connectToMySQL(server='mysql_ddi_dictionaries')
+    print('> Loading MySQL interaction dictionary')
+    sql = "SELECT id_drugbank_1, id_drugbank_2, text FROM drugbank510_interaction"
+    dfDDI = pd.read_sql(sql, engine)
+    dict_ddi_text = dfDDI.set_index(['id_drugbank_1', 'id_drugbank_2'])['text'].to_dict()
+    dict_ddi_text = {frozenset((k)): v for k, v in list(dict_ddi_text.items())}
+
+    #
+    # Load ADR
+    #
+    print('> Loading MySQL adverse-reaction dictionary')
+    sql = "SELECT id_drugbank, pt_code, placebos, frequencies, lowers, uppers FROM sider41_drug_has_sideeffect"
+    dfADR = pd.read_sql(sql, engine)
+    dfADR['pt_code'] = dfADR['pt_code'].astype(str)
+    dict_adr_freq = dfADR.set_index(['id_drugbank', 'pt_code'])['frequencies'].to_dict()
+    dict_adr_freq = {frozenset((k)): v for k, v in list(dict_adr_freq.items())}
+
+    #
+    # Load DI
+    #
+    print('> Loading MySQL drug-indication dictionary')
+    sql = "SELECT id_drugbank, pt_code, methods FROM sider41_drug_has_indication"
+    dfDI = pd.read_sql(sql, engine)
+    dict_di_meth = dfDI.set_index(['id_drugbank', 'pt_code'])['methods'].to_dict()
+    dict_di_meth = {frozenset((k)): v for k, v in list(dict_di_meth.items())}
+    """
+    #
+    # Load CoMention counts
+    #
+    rCSVfile = '{save_path:s}/02-instagram-epilepsy-comentions-{dicttimestamp:s}-samepost.csv.gz'.format(save_path=save_path, dicttimestamp=dicttimestamp)
+    df = pd.read_csv(rCSVfile, index_col=0, header=[0, 1])
+
+    mention_count_CSVfile = '{save_path:s}/02-instagram-epilepsy-mentions-counts-{dicttimestamp:s}-samepost.csv.gz'.format(save_path=save_path, dicttimestamp=dicttimestamp)
+    mention_count_df = pd.read_csv(mention_count_CSVfile, index_col=0)
+    mention_count_dict = {}
+    for _, row in mention_count_df.iterrows():
+        mention_count_dict[int(row['id_parent'])] = int(row['mention_count'])
+
+    # Filter
+    df = df.loc[df[('comention', 'count')] >= 3, :]
+
+    #
+    # build network
+    #
+    print('--- Building Network ---')
+    network_name = 'Network (Instagram : Epilepsy : {dicttimestamp:s})'.format(dicttimestamp=dicttimestamp)
+    G = nx.Graph(name=network_name)
+
+    # Adding Nodes/Edges
+    i = 0
+    n = len(df)
+    for _, row in df.iterrows():
+        if i % 1000 == 0:
+            print('> Parsing comentions. Row {i:,d} of {n:,d} ({per:.2%})'.format(i=i, n=n, per=(i / n)))
+        source = row['source']
+        target = row['target']
+        count = row['comention']['count']
+        #
+        if not G.has_node(source['id_parent']):
+            G.add_node(source['id_parent'], **{'parent': source['parent'], 'type': source['type']})
+        if not G.has_node(target['id_parent']):
+            G.add_node(target['id_parent'], **{'parent': target['parent'], 'type': target['type']})
+
+        G.add_edge(source['id_parent'], target['id_parent'], **{'count': count, 'is_original': True})
+        #
+        i += 1
+
+
+    print('--- Compute Normalized p_ij ---')
+
+    min_support = 10
+    print('> Normalized Values (min_support: {min_support:d}'.format(min_support=min_support))
+
+    def normalize(i, j, d, min_support=10):
+        r_ij = d['count']
+        r_ii = mention_count_dict[i]
+        r_jj = mention_count_dict[j]
+        if (r_ii + r_jj - r_ij) >= min_support:
+            return r_ij / (r_ii + r_jj - r_ij)
+        else:
+            return 0.
+
+    P = [normalize(i, j, d, min_support) for i, j, d in G.edges(data=True)]
+    P_dict = dict(list(zip(G.edges(), P)))
+    D_dict = dict(list(zip(G.edges(), list(map(utils.prox2dist, P)))))
+
+    # nx.set_edge_attributes(G, name='weight', values=P_dict) # weight = proximity
+    nx.set_edge_attributes(G, name='proximity', values=P_dict)
+    nx.set_edge_attributes(G, name='distance', values=D_dict)
+
+    if metric_backbone:
+        # Compute closure (Using the Dijkstra Class directly)
+        print('--- Computing Dijkstra APSP ---')
+
+        n = G.number_of_nodes()
+        i = 0
+        edges_seen = set()
+        for u, shortest_path_lenghts in all_pairs_dijkstra_path_length(G, weight='distance', disjunction=sum):
+            if (i % 50 == 0):
+                print('> Dijkstra: {i:d} of {n:d} ({per:.2%})'.format(i=i, n=n, per=(i / n)))
+            #
+            for v, shortest_path_legth in shortest_path_lenghts.items():
+                # Skip ij == ji and self-loops
+                if (u, v) in edges_seen or (u == v):
+                    continue
+
+                if not G.has_edge(u, v):
+                    # For now, don't add edges that do not exist in the original graph.
+                    # G.add_edge(u, v, **{'proximity': np.inf, 'distance': shortest_path_legth})
+                    pass
+                else:
+                    G[u][v]['metric_distance'] = shortest_path_legth
+                    G[u][v]['is_metric'] = True if ((shortest_path_legth == G[u][v]['distance']) and (shortest_path_legth != np.inf)) else False
+            #
+            i += 1
+
+
+        i = 0
+        edges_seen = set()
+        for u, shortest_path_lenghts in all_pairs_dijkstra_path_length(G, weight='distance', disjunction=max):
+            if (i % 50 == 0):
+                print('> Dijkstra: {i:d} of {n:d} ({per:.2%})'.format(i=i, n=n, per=(i / n)))
+            #
+            for v, shortest_path_legth in shortest_path_lenghts.items():
+                # Skip ij == ji and self-loops
+                if (u, v) in edges_seen or (u == v):
+                    continue
+
+                if not G.has_edge(u, v):
+                    # For now, don't add edges that do not exist in the original graph.
+                    # G.add_edge(u, v, **{'proximity': np.inf, 'distance': shortest_path_legth})
+                    pass
+                else:
+                    G[u][v]['ultrametric_distance'] = shortest_path_legth
+                    G[u][v]['is_ultrametric'] = True if ((shortest_path_legth == G[u][v]['distance']) and (shortest_path_legth != np.inf)) else False
+            #
+            i += 1
+
+        print('> Done.')
+
+        print('--- Calculating S Values ---')
+
+        S = {
+            (i, j): d['distance'] / d['metric_distance']
+            for i, j, d in G.edges(data=True)
+            if ((d.get('distance') < np.inf) and (d.get('metric_distance') > 0))
+        }
+        nx.set_edge_attributes(G, name='s_value', values=S)
+
+    """
+    print('--- Validating DDI/ADR/DI edges ---')
+    nr_ddi = 0
+    nr_adr = 0
+    nr_di = 0
+    for z, (i, j, d) in enumerate(G.edges(data=True), start=0):
+        # if (z % 10000==0):
+        #	print 'Validating DDI/ADR edge: %d of %d' % (z, G.number_of_edges() )
+
+        i_type = G.nodes[i]['type']
+        j_type = G.nodes[j]['type']
+        i_id_original_parent = G.nodes[i]['id_original_parent']
+        j_id_original_parent = G.nodes[j]['id_original_parent']
+
+        if (i_type in ['Drug', 'Allergen'] and j_type in ['Allergen', 'Drug']):
+            # DDI
+            if frozenset((i_id_original_parent, j_id_original_parent)) in dict_ddi_text:
+                G[i][j]['DDI'] = {'description': dict_ddi_text[frozenset((i_id_original_parent, j_id_original_parent))]}
+                nr_ddi += 1
+                print('> DDI found (%s so far)' % nr_ddi)
+
+        # ADR or DI?s
+        elif ((i_type in ['Drug', 'Allergen'] and j_type == 'Medical term') or (
+                i_type == 'Medical term' and j_type in ['Drug', 'Allergen'])):
+
+            # ADR
+            if frozenset((i_id_original_parent, j_id_original_parent)) in dict_adr_freq:
+                G[i][j]['ADR'] = {'frequency': dict_adr_freq[frozenset((i_id_original_parent, j_id_original_parent))]}
+                nr_adr += 1
+                print('> ADR found (%s so far)' % nr_adr)
+
+            # DI list
+            if frozenset((i_id_original_parent, j_id_original_parent)) in dict_di_meth:
+                G[i][j]['DI'] = {'method': dict_di_meth[frozenset((i_id_original_parent, j_id_original_parent))]}
+                nr_di += 1
+                print('> DI found (%s so far)' % nr_di)
+
+    print('> done.')
+    """
+    print('--- Exporting ---')
+    # Graph gormat
+    wGfile = '{save_path:s}/04-instagram-epilepsy-network-{dicttimestamp:s}-samepost'.format(save_path=save_path, dicttimestamp=dicttimestamp)
+    print('> gpickle')
+    nx.write_gpickle(G, wGfile + '.gpickle')
+    print('> graphml')
+    nx.write_graphml(G, wGfile + '.graphml')
+
+    # Nodes / Edges format
+    print('> edgelist (csv.gz)')
+    dfN = pd.DataFrame.from_dict(dict(G.nodes(data=True)), orient='index')
+    dfN.index.name = 'id_node'
+    sN = dfN.reset_index()['id_node']
+    sN.to_csv(wGfile + '-nodes.csv', index=False)
+    #
+    dfE = nx.to_pandas_edgelist(G)
+    if metric_backbone:
+        cols = ['source', 'target', 'count', 'proximity', 'distance', 'is_original', 'metric_distance', 'is_metric',
+                's_value', 'ultrametric_distance', 'is_ultrametric']
+    else:
+        cols = ['source', 'target', 'count', 'proximity', 'distance', 'is_original']
+    dfE = dfE[cols]
+    dfE.to_csv(wGfile + '-edges.csv', index=False)
+
+    print('Done.')
---
+++ instagram/alt_dict/count_comentions_samepost_alt.py
@@ -0,0 +1,137 @@
+# coding=utf-8
+# Author: Rion B Correia & Wuan Wang
+# Date: Jan 07, 2021
+#
+# Description: Loads the Mention file and builds Co-Mentions over the same post
+#
+import os
+import sys
+#
+include_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'include'))
+#include_path = '/nfs/nfs7/home/rionbr/myaura/include'
+sys.path.insert(0, include_path)
+#
+from ast import literal_eval
+import pandas as pd
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+from joblib import Parallel, delayed
+from itertools import combinations
+from collections import Counter, namedtuple
+import utils
+
+
+def compute_comentions(row):
+
+    list_post_comentions = []
+    id_user = row['id_user']
+
+    # Combition of all matches in a post
+    comb = list(combinations(row['matches'], 2))
+    if len(comb):
+
+        for source, target in comb:
+
+            # Skip self-loops
+            if source['id_parent'] == target['id_parent']:
+                continue
+
+            # Smaller id first (no direction)
+            if source['id'] > target['id']:
+                source, target = target, source
+
+            comention = {
+                'source': source,
+                'target': target,
+                'id_user': id_user,
+            }
+            comention['id_post'] = row['id_post']
+
+            list_post_comentions.append(comention)
+
+    return list_post_comentions
+
+
+def compute_instagram_comentions_samepost(drop_set, save_path):
+    #
+    # Init
+    #
+    dicttimestamp = '20180706'
+    # dicttimestamp = '20210321'
+
+    print('--- Building (same post) CoMentions ---')
+
+    print('--- Loading Mention Data ---')
+
+    rCSVfile = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'tmp-data/01-instagram-epilepsy-mentions-{dicttimestamp:s}.csv.gz'.format(dicttimestamp=dicttimestamp)))
+    df = pd.read_csv(rCSVfile, index_col=0, converters={'matches': literal_eval})
+
+    print('--- CoMention count ---')
+
+    i = 0
+    n = len(df)
+    # The co-mention counter 
+    counter = Counter()
+    # The mention counter
+    mention_count = Counter()
+    # A hashable object to use in the frozenset counter
+    #match = namedtuple('Match', ['id_match', 'id_parent', 'token', 'parent', 'type'])
+    matchparent = namedtuple('Match', ['id_parent', 'parent', 'type'])
+    #
+    for idx, row in df.iterrows():
+        if i % 10000 == 0:
+            print('> Computing comentions. Row {i:,d} of {n:,d} ({per:.2%})'.format(i=i, n=n, per=(i / n)))
+        #
+        matches = row['matches']
+        list_user_comentions = []
+        # row['matches'] is a list of dicts, each dict is a mention
+        # copy row['matches'] to a new list, remove any mention whose id is in drop_set
+        new_matches = [m for m in matches if m['id'] not in drop_set]
+        unique_id_parent = set([m['id_parent'] for m in new_matches])
+        mention_count.update(unique_id_parent)
+
+        unique_parent_rich = [matchparent(m['id_parent'], m['parent'], m['type']) for m in new_matches]
+        unique_parent_rich = list(set(unique_parent_rich))
+        for source, target in combinations(unique_parent_rich, 2):
+
+            # Skip self-loops
+            # if source['id_parent'] == target['id_parent']:
+            #     continue
+            # if source.id_parent == target.id_parent:
+            #     continue
+
+            #msource = match(source['id'], source['id_parent'], source['token'], source['parent'], source['type'])
+            #mtarget = match(target['id'], target['id_parent'], target['token'], target['parent'], target['type'])
+            # msource = matchparent(source['id_parent'], source['parent'], source['type'])
+            # mtarget = matchparent(target['id_parent'], target['parent'], target['type'])
+
+            # didn't use id_parent yet
+            comention = frozenset((source, target))
+            list_user_comentions.append(comention)
+        #
+        counter.update(list_user_comentions)
+        #
+        i += 1
+
+    print('Done.')
+
+    print('--- Convert to DataFrame and Export ---')
+    # Convert counter to Pandas DataFrame
+    records = [(s.id_parent, s.parent, s.type, t.id_parent, t.parent, t.type, c) for (s, t), c in counter.items()]
+    columns = pd.MultiIndex.from_tuples([(level, field) for level in ['source', 'target'] for field in ['id_parent', 'parent', 'type']] + [('comention', 'count')])
+    dfR = pd.DataFrame(records, columns=columns)
+
+    mention_count_df = pd.DataFrame(mention_count.items(), columns=('id_parent', 'mention_count'))
+
+    # Export
+    wCSVfile = '{save_path:s}/02-instagram-epilepsy-comentions-{dicttimestamp:s}-samepost.csv.gz'.format(save_path=save_path, dicttimestamp=dicttimestamp)
+    utils.ensurePathExists(wCSVfile)
+    dfR.to_csv(wCSVfile)
+
+
+    mention_count_CSVfile = '{save_path:s}/02-instagram-epilepsy-mentions-counts-{dicttimestamp:s}-samepost.csv.gz'.format(save_path=save_path, dicttimestamp=dicttimestamp)
+    utils.ensurePathExists(mention_count_CSVfile)
+    mention_count_df.to_csv(mention_count_CSVfile)
+
+    print('Done.')
